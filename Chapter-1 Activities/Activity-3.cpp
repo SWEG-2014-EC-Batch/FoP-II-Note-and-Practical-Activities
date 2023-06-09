221fa03e0f65feae1d58a368de008520507d5d49
Chere Lemma <chere.lemma@aastu.edu.et>
2023-06-09 09:41:15 +0300

    Update Activity-3.cpp

--- Chapter-1 Activities/Activity-3.cpp
+++ Chapter-1 Activities/Activity-3.cpp
@@ -1,1 +1,36 @@
+/* 
+* Problem Description - Write a function named mult() that accepts two floating-point numbers as a parameters,
+* multiplies these two numbers and display the result.
+* 
+* Purpose: To demonstrate function parameter
+*
+* Type of function: A function with parameters and no return.
+*/
 
+#include <iostream>
+using namespace std;
+void mult(float num1, float num2); // function prototype, declaration. this function has two parameter(s), but has no return.
+
+int main() {
+  mult(5.14, 13.65); // calling a function with floating-point literals
+  
+  float num;
+  cout<<"Enter a floating-point number: ";
+  cin>>num;
+  mult(num, 21.567); // calling a function with floating-point literal and variable.
+  
+  float num1, num2;
+  cout<<"Enter the first floating-point number: ";
+  cin>>num1;
+  cout<<"Enter the second floating-point number: ";
+  cin>>num2;
+  mult(num1, num2); // calling a function with floating-point literal and variable.
+  
+   return 0;
+}
+
+// function definition
+void farewell(float num1, float num2) // function header
+{
+    cout<<"The product of the two numbers is "<<num1*num3<<endl;
+}  
