6298be65da31083b888477c64c9a44015721adb9
Chere Lemma <chere.lemma@aastu.edu.et>
2023-06-15 09:09:57 +0300

    Update Exercise-9.cpp

--- Chapter-1 Activities/user_defined_header/Exercise-9.cpp
+++ Chapter-1 Activities/user_defined_header/Exercise-9.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include "calculator.h"
+using namespace std;
 
+int main() {
+    int num1, num2, res;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    
+    cout<<num1 <<" + "<<num2<<" = "<<addition(num1, num2)<<endl;
+    cout<<num1 <<" - "<<num2<<" = "<<substraction(num1, num2)<<endl;
+    cout<<num1 <<" * "<<num2<<" = "<<multiplication(num1, num2)<<endl;
+    cout<<num1 <<" / "<<num2<<" = "<<division(num1, num2)<<endl;
+    cout<<num1 <<" % "<<num2<<" = "<<reminder(num1, num2)<<endl;
+    
+    return 0;
+}
