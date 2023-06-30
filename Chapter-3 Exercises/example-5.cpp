a9f4b729b6b9a93c3a7f6e3942359c98c8a5e6aa
Chere Lemma <chere.lemma@aastu.edu.et>
2023-06-30 20:05:16 +0300

    Update example-5.cpp

--- Chapter-3 Exercises/example-5.cpp
+++ Chapter-3 Exercises/example-5.cpp
@@ -1,1 +1,39 @@
+#include <iostream> 
+#include <fstream>  
+using namespace std;
+
+// define the struct Student
+struct Student{
+  int roll;
+  char name[25];
+  float marks;
+} stud;
+
+// print the student details to console
+void putData()
+{
+  cout<<"\n"<<stud.Roll;
+  cout<<"\t"<<stud.Name;
+  cout<<"\t"<<stud.Marks;
+}
+
+void read_file()
+{
+  fstream inf;
+  inf.open("Student.dat", ios::in|ios::binary); // open file in binary read mode
+
+  cout<<"\n\tRoll\tName\tMarks\n";
+
+  inf.read( (char *) &Stu, sizeof(stud) ); // read first object from file
+
+  while(inf != NULL){
+    putData();  // print the object to console
+    inf.read( (char *) &Stu, sizeof(stud) ); // read next object from file
+  }
+  inf.close();
+}
+
+int main() {
+   read_file ();
+}
 
